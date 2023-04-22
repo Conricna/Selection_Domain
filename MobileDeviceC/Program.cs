7682bf69051d11c305c2651cd0faf84266a69895
wasilisk <walbynow@gmail.com>
2023-04-22 16:25:20 +0300

    Добавьте файлы проекта.

---
+++ MobileDeviceC/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MobileDeviceC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(Solution.Arg());
+            Console.WriteLine("\nВыберите домен по которому хотите совершть переход");
+            ///               {                          visitwar.com                   fruonline.co.uk    australia.open.com    credit.card.us  }
+            string[] array1 = { "unlock.microvirus.md", "visitwar.com", "visitwar.de", "fruonline.co.uk", "australia.open.com", "credit.card.us" };
+            string[] array2 = { "microvirus.md", "visitwar.de", "piratebay.co.uk", "list.stolen.credit.card.us" };
+            for (int i = 1; i < array1.Length+1; i++)
+            {
+                Console.WriteLine(i + ": " + array1[i-1]);
+            }
+            Console.Write("\n По этим доменам можно совершить переход: ");
+            Console.WriteLine(string.Join("  ", Solution.solution(array1, array2)));
+
+
+            /*                                   Тут предлогается пользователю выбрать доиен по которому он хотел бы перейти
+             *                                             нужно только их расскоментить 
+             *                                                                            Спасибо вам, мне понравилост)))
+            Console.Write("\nВаш выбор - ");
+            int choise = Convert.ToInt32(Console.ReadLine())-1;  
+            Console.WriteLine(string.Join(" \n ", Solution.solutionans(array1, array2, choise)));
+            */
+        }
+    }
+}
---
+++ MobileDeviceC/Solution.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace MobileDeviceC
+{
+    internal class Solution
+    {
+        public static String Arg()
+        {
+            String answer ="Hello, World!";
+            return answer;
+        }
+       
+        public static int[] solution(string[] A, string[] B )
+        {
+            int[] allowed = new int[A.Length];
+
+            for (int i = 0; i < allowed.Length; i++)
+            {
+                allowed[i] = i+1;
+            }
+            int count = 0;
+            for (int i = 0; i < B.Length; i++)
+            {
+                for (int j = 0; j < A.Length; j++)
+                {
+
+                    Regex regex = new Regex($@"{B[i]}");
+                    MatchCollection matches = regex.Matches(A[j]);
+                    if (matches.Count > 0)
+                    {
+                        allowed[j] = -1;                       
+                        ++count;
+                        break;
+                    }
+                }
+            }
+            allowed = allowed.Where(e => e != -1).ToArray();
+            Array.Resize(ref allowed, A.Length - count);
+            
+            return allowed;
+        }
+
+        //                              Ниже тот самый фрагмент, который отвечает за выбор пользователя 
+        
+        public static string solutionans(string[] A, string[] B, int numarr)
+        {
+            string answer = "На данный момент работа завершена :)";
+            int count = 0;
+            string domen = "";
+            for (int i = 0; i < B.Length; i++)
+            {
+                Regex regex = new Regex($@"{B[i]}");
+                try
+                {
+                    MatchCollection matches = regex.Matches(A[numarr]);
+
+                    if (matches.Count > 0)
+                    {
+                        domen = B[i];
+                        ++count;
+                        break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Вы выбрали не допустимый домен!\n");
+                    ++count;
+                    return answer;
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("\nПереход можно выполнить\n");
+            else
+                Console.WriteLine($"\nДоступ к домену ограничен {domen} \n");
+
+
+
+            return answer;
+        }
+    }
+}
